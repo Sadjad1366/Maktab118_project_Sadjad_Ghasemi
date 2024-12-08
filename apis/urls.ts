a4fbb635cc94e6bb0592918d4dd9e56f18845ccc
sadjad1366 <sadjad.ghasemi@gmail.com>
2024-12-08 21:57:53 +0330

    urls fixed

--- apis/urls.ts
+++ apis/urls.ts
@@ -1,19 +1,11 @@
 export const urls = {
-<<<<<<< HEAD
-  auth:{adminLog: "/auth/login",
-    tokenGen:"/auth/token"
-  },
-  product: "/products",
-=======
   auth:{
-admin: "/auth/login"
-  }
-  ,
+   admin: "/auth/login"
+  },
   product:{
     getAll:"/products",
     getById: (id: string) => `/products/${id}`
   } ,
->>>>>>> feature/product-card
   order: "/orders",
   user: "/users",
   category: "/categories",
