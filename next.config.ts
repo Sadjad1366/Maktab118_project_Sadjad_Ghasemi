513d983f446ea1b78188570567c622ccace4881f
sadjad1366 <sadjad.ghasemi@gmail.com>
2025-02-04 01:10:02 +0330

    change nexConfig.ts

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,16 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
+        pathname: "/images/products/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "backend-app-gamma.vercel.app",
         pathname: "/images/products/images/**",
       },
     ],
